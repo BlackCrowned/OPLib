f2638a19a5510022b3381b7b8e91ea41b18a8707
Michael Moehrle <mi.moehrle@gmail.com>
2014-01-28 00:04:22 +0100

    .each() | .attr() | Kommentare | etc.
    HINZUGEFÜGT:
    .each(context, fn, args)
    Überschreibungssicherung von "$" und "OPLib"
    
    GEÄNDERT:
    .attr(type, name, properties)
    Kommentare

--- OPLib.js
+++ OPLib.js
@@ -8,134 +8,172 @@ var oplib = (function() {
 
     oplib.fn = oplib.prototype = {
         constructor : oplib,
-        Init : function(selector, context) {//Elemente auswählen
-            this.elems = oplib.fn.ElementSelection(selector, context);
+        //Elemente auswählen
+        Init : function(selector, context) {
             //Ausgewählte Elemente zuweisen
+            this.elems = oplib.fn.ElementSelection(selector, context);
+
             this.context = undefined;
             this.length = undefined;
             return this;
         },
         attr : function(type, name, property) {
+            var args = {};
+            
+            /*
+             * TYPE: 1 == "set" "add"
+             * TYPE: 2 == "remove" "delete"
+             * TYPE: 3 == "get"
+             */
             //TODO
+            oplib.fn.each(this, function(elem) {
+
+            }, this.elems);
         }
     };
 
-    oplib.fn.merge = function(obj, probs) {//Objecte zusammenführen
-        for (var i in probs) {//For-schleife für alle Elemente in probs
-            obj[i] = probs[i];
+    //Objecte zusammenführen
+    oplib.fn.merge = function(obj, probs) {
+        //For-schleife für alle Elemente in probs
+        for (var i in probs) {
             //Elemente aus probs obj zuweisen
+            obj[i] = probs[i];
+
         }
-        return obj;
         //Zusammengeführtes obj zurückgeben
+        return obj;
+
     };
 
-    oplib.fn.extend = function(obj, probs) {//Eine Funtion oder oplib.fn erweitern
-        if (arguments.length == 1) {//Nur ein argument angegeben? -> oplib erweitern
+    //Eine Funtion oder oplib.fn erweitern
+    oplib.fn.extend = function(obj, probs) {
+        //Nur ein argument angegeben? -> oplib erweitern
+        if (arguments.length == 1) {
             props = arguments[0];
-            //
             obj = this;
-            //
         }
-        return this.merge(obj, probs);
         //Objecte zusammenführen und zurückgeben
+        return this.merge(obj, probs);
+
     };
 
-    oplib.fn.Init.prototype = oplib.fn;
     //oplib.fn.Init besitzt den gleichen Prototyp wie oplib
-    oplib.fn.ClassRegex = /\.\w*/;
+    oplib.fn.Init.prototype = oplib.fn;
     //Regex für Klassen Selectoren
-    oplib.fn.IdRegex = /#\w*/;
+    oplib.fn.ClassRegex = /\.\w*/;
     //Regex für ID Selectoren
-    oplib.fn.HtmlTagRegex = /<\w*>/;
+    oplib.fn.IdRegex = /#\w*/;
     //Regex für HTML-Tag Selectoren
-    oplib.fn.PossibleSelectorsRegex = /[.#<]/;
+    oplib.fn.HtmlTagRegex = /<\w*>/;
     //Regex für alle möglichen Selectoren
-    oplib.fn.ElementSelection = function(selector, context) {//Selectiert die Entsprechenden Elemente
+    oplib.fn.PossibleSelectorsRegex = /[.#<]/;
+
+    //Selectiert die Entsprechenden Elemente
+    oplib.fn.ElementSelection = function(selector, context) {
         var elems;
+        //Context zuweisen
         context = oplib.fn.ElementSelection.prototype.DOMObjectFromSelector(context);
-
+        //Gewählte Elemente zuweisen
         elems = oplib.fn.ElementSelection.prototype.DOMObjectFromSelector(selector, context);
-
+        //Console Log
         console.log("Selected: " + elems + " in " + context);
-
+        //Ausgewählte Elemente zurückgeben
         return elems;
     };
 
+    //Wandelt einen Selector in ein DOMObject um
     oplib.fn.ElementSelection.prototype.DOMObjectFromSelector = function(selector, context) {
         var elems = [];
 
-        if (!selector) {//Wurde ein selector übergeben
-            selector = document.body;
+        //Wurde ein selector übergeben
+        if (!selector) {
             //Standart Selector = document.body
+            selector = document.body;
+
         }
 
-        if (!context) {//Wurde ein context übergeben
-            context = document.body;
+        //Wurde ein context übergeben
+        if (!context) {
             //Standart Context = document.body
+            context = document.body;
+
         }
 
-        if ( selector instanceof Node) {//Ist selector bereits ein DOMObject?
-            elems.push(selector);
+        //Ist selector bereits ein DOMObject?
+        if ( selector instanceof Node) {
             //Element = Selector
-        } else if ( selector instanceof NodeList) {
+            elems.push(selector);
+
+        }
+        else if ( selector instanceof NodeList) {
             for (var i = 0; i < selector.length; i++) {
                 elems.push(oplib.fn.ElementSelection.prototype.DOMObjectFromSelector(selector[i]));
             }
-        } else if ( typeof selector === "string") {//Ist Selector ein String, um Regexausdrücke anzuwenden?
-            if (oplib.fn.ClassRegex.test(selector)) {//Enthält selector einen Klassenausdruck?
-                var _selector = "";
+        }
+        //Ist Selector ein String, um Regexausdrücke anzuwenden?
+        else if ( typeof selector === "string") {
+            //Enthält selector einen Klassenausdruck?
+            if (oplib.fn.ClassRegex.test(selector)) {
                 //Variable für .Class Selector String
-                var startPos = selector.search(oplib.fn.ClassRegex);
+                var _selector = "";
                 //Position des Klassen Selectors
-                _selector = selector = selector.slice(startPos + 1, selector.length);
+                var startPos = selector.search(oplib.fn.ClassRegex);
                 //Klassen Selector entfernen
-                startPos = _selector.search(oplib.fn.PossibleSelectorsRegex);
+                _selector = selector = selector.slice(startPos + 1, selector.length);
                 //Position der Möglichen weiteren Selectoren
-                if (startPos != -1) {//Keine weiteren Selectoren gefunden!
-                    _selector = selector.slice(startPos, selector.length);
+                startPos = _selector.search(oplib.fn.PossibleSelectorsRegex);
+                //Weitere Selectoren gefunden!
+                if (startPos != -1) {
                     //weitere Selectoren sichern
-                    selector = selector.slice(0, startPos);
+                    _selector = selector.slice(startPos, selector.length);
                     //weitere Selectoren entfernen
-                } else {
+                    selector = selector.slice(0, startPos);
+                }
+                //Keine weiteren Selectoren gefunden
+                else {
                     _selector = "";
                 }
-                console.log("Processing CLASS-selector: " + selector);
-                for (var i = 0; i < context[0].getElementsByClassName(selector).length; i++) {//Elemente die auf selector passen im Context wählen
+                //Elemente die auf selector passen im Context wählen
+                for (var i = 0; i < context[0].getElementsByClassName(selector).length; i++) {
                     elems.push(context[0].getElementsByClassName(selector)[i]);
                 }
 
                 if (_selector != "") {
-                    console.log("Additional Selector spezified: " + _selector);
-                    elems.sameElements(oplib.fn.ElementSelection.prototype.DOMObjectFromSelector(_selector));
                     //Übereinstimmende Elemente übernehmen
+                    elems.sameElements(oplib.fn.ElementSelection.prototype.DOMObjectFromSelector(_selector));
                 }
-            } else if (oplib.fn.IdRegex.test(selector)) {//Enthält selector einen IDausdruck?
-                var _selector = "";
+            }
+            //Enthält selector einen IDausdruck?
+            else if (oplib.fn.IdRegex.test(selector)) {
                 //Variable für #ID Selector String
-                var startPos = selector.search(oplib.fn.IdRegex);
+                var _selector = "";
                 //Position des ID Selectors
-                _selector = selector = selector.slice(startPos + 1, selector.length);
+                var startPos = selector.search(oplib.fn.IdRegex);
                 //ID Selector entfernen
-                startPos = _selector.search(oplib.fn.PossibleSelectorsRegex);
+                _selector = selector = selector.slice(startPos + 1, selector.length);
                 //Position der Möglichen weiteren Selectoren
-                if (startPos != -1) {//Keine weiteren Selectoren gefunden!
-                    _selector = selector.slice(startPos, selector.length);
+                startPos = _selector.search(oplib.fn.PossibleSelectorsRegex);
+                //Weitere Selectoren gefunden!
+                if (startPos != -1) {
                     //weitere Selectoren sichern
-                    selector = selector.slice(0, startPos);
+                    _selector = selector.slice(startPos, selector.length);
                     //weitere Selectoren entfernen
-                } else {
+                    selector = selector.slice(0, startPos);
+                }
+                //Keine weiteren Selectoren gefunden
+                else {
                     _selector = "";
                 }
-                console.log("Processing ID-selector: " + selector);
-                elems.push(document.getElementById(selector));
                 //Elemente die auf selector passen in wählen, kann nur im gesamten document gewählt werden
+                elems.push(document.getElementById(selector));
 
                 if (_selector != "") {
-                    console.log("Additional Selector spezified: " + _selector);
-                    elems.sameElements(oplib.fn.ElementSelection.prototype.DOMObjectFromSelector(_selector));
                     //Übereinstimmende Elemente übernehmen
+                    elems.sameElements(oplib.fn.ElementSelection.prototype.DOMObjectFromSelector(_selector));
                 }
-            } else if (oplib.fn.HtmlTagRegex.test(selector)) {//Enthält selector ein HtmlTag?
+            }
+            //Enthält selector ein HtmlTag?
+            else if (oplib.fn.HtmlTagRegex.test(selector)) {
                 //TODO HTML TAG SELECTOR
             }
         }
@@ -144,82 +182,101 @@ var oplib = (function() {
         return elems;
     };
 
-    oplib.fn.extend(Object.prototype, {//Object erweitern
-        compare : function(obj1, obj2) {//.compare
-            if (arguments.length == 1) {//Nur ein Argument angegeben? -> Dieses Object mit Argument vergleichen
+    //Funktion in einem bestimmten Context mit verschiedenen Argumenten ausführen
+    oplib.fn.each = function(context, fn, args) {
+        //Alle Argument für die Funktion durchgehen
+        for (var i in args) {
+            //Funktion mit Argument[i] im Context ausführen
+            fn.apply(context, args[i]);
+        }
+    };
+
+    //Object erweitern
+    oplib.fn.extend(Object.prototype, {
+        compare : function(obj1, obj2) {
+            //Nur ein Argument angegeben? -> Dieses Object mit Argument vergleichen
+            if (arguments.length == 1) {
                 obj2 = arguments[0];
-                //
                 obj1 = this;
-                //
             };
-            var newObj = new Object();
-            //Neues Object, das gleiche Elemente enthält
-            for (var i in obj1) {//Forschleife mit allen Elementen von abj1
-                if (obj2[i]) {//Element von obj1 auch in ob2?
-                    if (obj1[i] == obj2[i]) {//Elemente von obj1 und obj2 gleich
-                        newObj[i] = obj1[i];
+            var newObj = {};
+            //Forschleife mit allen Elementen von abj1
+            for (var i in obj1) {
+                //Element von obj1 auch in ob2?
+                if (obj2[i]) {
+                    //Elemente von obj1 und obj2 gleich
+                    if (obj1[i] == obj2[i]) {
                         //Elemente newObj zuordnen
+                        newObj[i] = obj1[i];
                     }
                 };
             };
-            if (arguments.length == 1) {//Nur ein Argument angegeben? -> Object soll sich selbs verändern
-                for (var i in this) {//Alle eigenen Elemente löschen
+            //Nur ein Argument angegeben? -> Object soll sich selbs verändern
+            if (arguments.length == 1) {
+                //Alle eigenen Elemente löschen
+                for (var i in this) {
                     delete this[i];
-                    //
 
                 }
                 for (var i in newObj) {//Gemeinsamme Elemente hinzufügen
                     this[i] = newObj[i];
-                    //
                 }
             }
             return newObj;
-            //newObj zurückgeben
         },
-        merge : oplib.fn.merge, //Merge für alle Obejects freigeben
-        extend : oplib.fn.extend //Extend für alle Objecte freigeben
+        //Merge für alle Obejects freigeben
+        merge : oplib.fn.merge,
+        //Extend für alle Objecte freigeben
+        extend : oplib.fn.extend
     });
 
-    oplib.fn.extend(Array.prototype, {//Array erweitern
-        sameElements : function(arr1, arr2) {//.sameElements
-            if (arguments.length == 1) {//Nur ein Argument angegeben? -> Dieses Object mit Argument vergleichen
+    //Array erweitern
+    oplib.fn.extend(Array.prototype, {
+        sameElements : function(arr1, arr2) {
+            //Nur ein Argument angegeben? -> Dieses Object mit Argument vergleichen
+            if (arguments.length == 1) {
                 arr2 = arguments[0];
-                //
                 arr1 = this;
-                //
             }
             var newArr = [];
-            //Neues Array, das gleiche Elemente enthält
-            for (var i = 0; i < arr1.length; i++) {//Forschleife mit allen Elementen von arr1
-                for (var a = 0; a < arr2.length; a++) {//Forschleife mit allen Elementen von arr2
-                    console.log(arr1[i] + " -- " + arr2[a]);
-                    if (arr1[i] == arr2[a]) {//Sind gleiche Elemente in arr1 und arr2 vorhanden
-                        newArray.push(arr1[i]);
+            //Forschleife mit allen Elementen von arr1
+            for (var i = 0; i < arr1.length; i++) {
+                //Forschleife mit allen Elementen von arr2
+                for (var a = 0; a < arr2.length; a++) {
+                    //Sind gleiche Elemente in arr1 und arr2 vorhanden
+                    if (arr1[i] == arr2[a]) {
                         //Ja, dem neuen Array hinzufügen
+                        newArr.push(arr1[i]);
                     }
                 }
             }
-            if (arguments.length == 1) {//Nur ein Argument angegeben?
-                this.splice(0, this.length);
+            //Nur ein Argument angegeben?
+            if (arguments.length == 1) {
                 //Alle eigenen Elemente löschen
-                for (var i = 0; i < newArr.length; i++) {//
-                    this.push(newArr[i]);
+                this.splice(0, this.length);
+                //Neue Elemente dem eigenen Array zuweisen
+                for (var i = 0; i < newArr.length; i++) {
                     //Neue Elemente dem eigenen Array zuweisen
+                    this.push(newArr[i]);
+                    
                 }
             }
             return newArr;
-            //newArr zurückgeben
         }
     });
 
-    if (!window.console) {//Debugging Console - Bugfix for IE
+//Debugging Console - Bugfix for IE
+    if (!window.console) {
         window.console = {
+            //Erstellt leere Funktion um IE-Crash zu verhindern
             log : function() {
             }
         };
-        //Erstellt leere Funktion um IE-Crash zu verhindern
+        
     }
-
+    
+    window._OPLib = window.OPLib;
+    window._$ = window.$;
     window.OPLib = oplib;
     window.$ = oplib;
 })();
