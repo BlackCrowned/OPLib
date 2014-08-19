520d2cf301c8f5f2d658e84820d51500bb00b708
Michael Möhrle <mi.moehrle@gmail.com>
2014-08-19 17:06:42 +0200

    .FormBefore, etc.. hinzugefügt

--- OPLib.js
+++ OPLib.js
@@ -3217,6 +3217,26 @@ var oplib = (function() {
 		return this;
 	};
 
+	oplib.fb.FormBefore = function(id, before) {
+		oplib.fn.Form.before(id, before, this);
+		return this;
+	};
+
+	oplib.fb.FormAfter = function(id, after) {
+		oplib.fn.Form.after(id, after, this);
+		return this;
+	};
+
+	oplib.fb.FormFirst = function(id) {
+		oplib.fn.Form.first(id, this);
+		return this;
+	};
+
+	oplib.fb.FormLast = function(id) {
+		oplib.fn.Form.last(id, this);
+		return this;
+	};
+
 	oplib.fn.Form.addData = function(type, data, elem) {
 		if (!elem.oplib.Form.data[type]) {
 			elem.oplib.data[type] = [];
